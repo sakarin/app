6f6b6dd03a8e0aa42e41a1f7cee900ae0790897f
Sakarin Radchapunya <sakarin.my@gmail.com>
2011-12-25 22:06:26 +0700

    done

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -94,6 +94,13 @@ class CheckoutController < Spree::BaseController
 
   def before_payment
     current_order.payments.destroy_all if request.put?
+
+    if Rails.env.production?
+      @payment_method = Preference.where(:value => request.host).first
+    else
+      @payment_method = Preference.where(:value => request.host_with_port).first
+    end
+
   end
 
   def after_complete
