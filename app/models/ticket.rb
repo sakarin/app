0a41047c9c2e69c3c1a42bef878ce5ade0d55b58
Sakarin Radchapunya <sakarin.my@gmail.com>
2012-01-06 20:30:09 +0700

    - edit Editor

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -8,6 +8,9 @@ class Ticket < ActiveRecord::Base
 
   has_attached_file :attachment, :path => ":rails_root/public/assets/tickets/:id/:basename.:extension"
 
+  validates_attachment_size :attachment, :less_than => 2.megabytes
+  validates_attachment_content_type :attachment, :content_type => ['image/jpeg', 'image/png', 'image/gif']
+
   scope :by_customer, lambda {|customer| joins(:user).where("users.email =?", customer)}
 
   validates_presence_of :title
