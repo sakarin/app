0d894e9de803eae32114f3edce059d9680e4d9cb
Sakarin Radchapunya <sakarin.my@gmail.com>
2012-04-24 15:07:15 +0700

    fix paypal and multi-currency done

--- vendor/extensions/spree_paypal_express/app/controllers/checkout_controller_decorator.rb
+++ vendor/extensions/spree_paypal_express/app/controllers/checkout_controller_decorator.rb
@@ -258,7 +258,7 @@ CheckoutController.class_eval do
 
   # hook to override paypal site options
   def paypal_site_opts
-    {  }
+    {  :currency => session[:currency_id].present? ? session[:currency_id].to_s : Preference.find_by_owner_id_and_name(payment_method, 'currency').value }
   end
 
   def order_opts(order, payment_method, stage)
@@ -301,8 +301,8 @@ CheckoutController.class_eval do
              :subtotal          => ((order.item_total * 100) + credits_total).to_i,
              :tax               => ((order.adjustments.map { |a| a.amount if ( a.source_type == 'Order' && a.label == 'Tax') }.compact.sum) * 100 ).to_i,
              :shipping          => ((order.adjustments.map { |a| a.amount if a.source_type == 'Shipment' }.compact.sum) * 100 ).to_i,
-             :money             => (order.total * 100 ).to_i,
-             :currency => session[:currency_id].present? ? session[:currency_id].to_s : Preference.find_by_owner_id_and_name(payment_method, 'currency').value
+             :money             => (order.total * 100 ).to_i
+
 
     }
 
@@ -313,10 +313,10 @@ CheckoutController.class_eval do
     subtotal = 0
     opts[:items].each do |item|
       subtotal += (item[:amount].to_i * item[:quantity])
-      logger.debug "#{item[:amount]}"
     end
 
     opts[:subtotal] = subtotal.to_i
+    opts[:money] = opts[:subtotal] + opts[:shipping]
 
     if stage == "checkout"
       opts[:handling] = 0
@@ -327,8 +327,8 @@ CheckoutController.class_eval do
       #hack to add float rounding difference in as handling fee - prevents PayPal from rejecting orders
       #because the integer totals are different from the float based total. This is temporary and will be
       #removed once Spree's currency values are persisted as integers (normally only 1c)
-      opts[:handling] =  (order.total*100).to_i - opts.slice(:subtotal, :tax, :shipping).values.sum
-
+      #opts[:handling] =  ((order.total*100).to_i - opts.slice(:subtotal, :tax, :shipping).values.sum)
+      opts[:handling] = 0
 
     end
 
